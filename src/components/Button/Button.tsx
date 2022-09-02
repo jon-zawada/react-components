b9fa1a5a847b20c761ddb35dade3323bbbbe7e2d
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-02 16:21:24 -0700

    css implementedx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Button.css';
 
 interface ButtonProps {
   label: string;
+  size: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={props.size}>{props.label}</button>;
 };
 
 export default Button;
